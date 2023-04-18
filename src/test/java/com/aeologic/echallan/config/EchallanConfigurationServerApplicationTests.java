02bf34d9d61e2e74583e32c24a9794f1650546f5
Anilesh Mishra <anileshmishra.mishra99@gmail.com>
2023-04-19 00:05:06 +0530

    First Commit - Configuration Server

---
+++ src/test/java/com/aeologic/echallan/config/EchallanConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeologic.echallan.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchallanConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
